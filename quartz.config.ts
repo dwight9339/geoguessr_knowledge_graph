b510135d48c1b8e38c2e02d79cb5b2e7b38fb504
David White <whited9339@gmail.com>
2025-05-24 12:16:13 -0600

    Updated favicon and page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "D'wight's Geo Notebook",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
